abc41e63374f1d22fb4cf1edac1148bb9ee61abd
grace-ko <gracekodes@gmail.com>
2021-06-20 20:51:01 -0700

    started setting up db

--- server/index.js
+++ server/index.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    }
-  )
-);
+mongoose.connect(keys.mongoURI);
+const app = express();
 
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
