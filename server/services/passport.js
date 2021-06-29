319a91bc86fee20680bdcdd08110cc850961dedb
grace-ko <gracekodes@gmail.com>
2021-06-29 06:30:11 -0700

    set up user db with mongo and mongoose, added user creation to passport callback

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,7 +17,15 @@ passport.use(
     callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
